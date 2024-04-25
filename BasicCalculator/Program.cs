1c8080714ae91316d3f47b19f5b668b59ea5ae9d
Cody <codywahl@gmail.com>
2024-04-25 14:36:39 +0900

    progress?

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -4,9 +4,33 @@ namespace BasicCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution mySolution = new Solution();
+            Console.WriteLine(mySolution.calculate("1 + 1"));
+        }
+    }
+
+    public class Solution {
+        private int currentIndex = 0;
+
+        public int calculate(String inputStr){
+            int currentTotal = 0;
+            
+            while(currentIndex < inputStr.Length){
+                char currentCharacter = inputStr[currentIndex];
+
+                if(Char.IsDigit(currentCharacter)){
+                    currentTotal += currentCharacter;
+                }
+                if(inputStr[currentIndex] == ')'){
+                    return currentTotal;
+                }
+            }
+
+            currentIndex++; 
+            return 0;
         }
     }
 }
